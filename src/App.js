500628365a40f46b743a13ceb1f39da3507c8ece
Dhanraj Bhedi <dhanraj0709@gmail.com>
2023-02-22 11:37:36 +0530

    Initialize React Project and added Directory, category item component

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import Directory from './components/directory/directory.component';
+
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageUrl: 'https://i.ibb.co/5KVN2mT/hats.png',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/5BLVkFV/jackets.png',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageUrl: 'https://i.ibb.co/XYJG9GR/sneaker.png',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageUrl: 'https://i.ibb.co/Wz3NMwN/womens.png',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageUrl: 'https://i.ibb.co/7W7WF22/men.png',
+    },
+  ];
+
+  return <Directory categories={categories} />;
+}
+
+export default App;
